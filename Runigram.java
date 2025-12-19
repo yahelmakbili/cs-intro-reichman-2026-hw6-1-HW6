4d29943268199ead29769ab51d9fda7c8b93d556
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-12-19 19:14:41 +0200

    fix2

--- Runigram.java
+++ Runigram.java
@@ -108,7 +108,7 @@ public class Runigram {
 		int r = pixel.getRed();
 		int g = pixel.getGreen();
 		int b = pixel.getBlue();
-		int lum = (int) (0.299 * r + 0.587 * g + 0.114 * b);
+		int lum = (int) Math.round(0.299 * r + 0.587 * g + 0.114 * b);
 		return new Color(lum, lum, lum);
 	}
 
